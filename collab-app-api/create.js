a17cb7d8e56c570364a74905d898d870e0ead7a4
yossaj <yossaj@icloud.com>
2019-08-08 23:32:57 +0100

    refactored

--- collab-app-api/create.js
+++ collab-app-api/create.js
@@ -1,11 +1,9 @@
 import uuid from "uuid";
-import AWS from "aws-sdk";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export function main(event, context, callback) {
+export async function main(event, context) {
     const data = JSON.parse(event.body);
-
     const params = {
         TableName: "projects",
         Item: {
@@ -19,28 +17,10 @@ export function main(event, context, callback) {
         }
     };
 
-    dynamoDb.put(params, (error, data) => {
-        // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-        const headers = {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true
-        };
-
-        if (error) {
-            const response = {
-                statusCode: 500,
-                headers: headers,
-                body: JSON.stringify({ status: false })
-            };
-            callback(null, response);
-            return;
-        }
-
-        const response = {
-            statusCode: 200,
-            headers: headers,
-            body: JSON.stringify(params.Item)
-        };
-        callback(null, response);
-    });
+    try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+    } catch (e) {
+        return failure({ status: false });
+    }
 }
