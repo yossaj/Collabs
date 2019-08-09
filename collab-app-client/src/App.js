33fae0c6584d39175c4a790d8259ab7a0e1d1eef
yossaj <yossaj@icloud.com>
2019-08-09 19:09:29 +0100

    inital front-end setup

--- collab-app-client/src/App.js
+++ collab-app-client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import Routes from "./Routes";
+import { LinkContainer } from "react-router-bootstrap";
 import "./App.css";
 
 class App extends Component {
@@ -17,8 +18,12 @@ class App extends Component {
           </Navbar.Header>
           <Navbar.Collapse>
             <Nav pullRight>
-              <NavItem href="/signup">Signup</NavItem>
-              <NavItem href="/login">Login</NavItem>
+              <LinkContainer to="/signup">
+                <NavItem>Signup</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <NavItem>Login</NavItem>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
---
+++ collab-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "eu-west-1",
+        BUCKET: "collab-bucket1"
+    },
+    apiGateway: {
+        REGION: "eu-west-1",
+        URL: "https://d7bxwldltc.execute-api.eu-west-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "eu-west-1",
+        USER_POOL_ID: "eu-west-1_JkG59iULq",
+        APP_CLIENT_ID: "7876kuacd4hv9bc7ef80hamm27",
+        IDENTITY_POOL_ID: "eu-west-1:afd4d7a6-7066-4d42-9997-adaeb6fc0951"
+    }
+};
