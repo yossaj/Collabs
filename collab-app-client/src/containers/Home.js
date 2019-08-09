6dd4b53b6d42b738bc6753899ec9eb60cf56e350
yossaj <yossaj@icloud.com>
2019-08-09 18:48:15 +0100

    added routes and sign/log in holders

---
+++ collab-app-client/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className="Home">
+                <div className="lander">
+                    <h1>Collaborate</h1>
+                    <p>Find people to collaborate with.</p>
+                </div>
+            </div>
+        );
+    }
+}
