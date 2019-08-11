9c387f618a7e4eb1875509ce700f60420cc378b3
yossaj <yossaj@icloud.com>
2019-08-11 18:57:45 +0100

    changes to home display

--- collab-app-client/src/containers/Home.js
+++ collab-app-client/src/containers/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { PageHeader, ListGroup, ListGroupItem, PanelTitle} from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { API } from "aws-amplify";
@@ -35,27 +35,40 @@ export default class Home extends Component {
 
 
     renderProjectsList(projects) {
+        console.log(projects)
         return [{}].concat(projects).map(
             (project, i) =>
                 i !== 0
-                    ? <LinkContainer
-                        key={project.projectid}
-                        to={`/projects/${project.projectid}`}
-                    >
-                        <ListGroupItem header={project.description.trim().split("\n")[0]}>
-                            {"Created: " + new Date(project.createdAt).toLocaleString()}
-                        </ListGroupItem>
-                    </LinkContainer>
-                    : <LinkContainer
-                        key="new"
-                        to="/projects/new"
-                    >
-                        <ListGroupItem>
-                            <h4>
-                                <b>{"\uFF0B"}</b> Create a new project
-              </h4>
-                        </ListGroupItem>
-                    </LinkContainer>
+                    ?
+                    <Fragment>
+                        <div className="col-lg-3 col-md-6 mb-4">
+                            <div className="card h-100">
+                                <img className="card-img-top" src="http://placehold.it/500x325" alt="" />
+                                <div className="card-body">
+                                    <h4 className="card-title">{project.description}</h4>
+                                </div>
+                                <div className="card-footer">
+                                    <a href="#" className="btn btn-primary">Help Out!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </Fragment>
+                    :
+                    <Fragment>
+                        <div className="col-lg-3 col-md-6 mb-4">
+                            <div className="card h-100">
+                                <img className="card-img-top" src="http://placehold.it/500x325" alt="" />
+                                <div className="card-body">
+                                    <h4 className="card-title">Charity Project</h4>
+                                </div>
+                                <div className="card-footer">
+                                    <a href="#" className="btn btn-primary">Help Out!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </Fragment>
+                    
+  
         );
     }
 
@@ -80,17 +93,18 @@ export default class Home extends Component {
                         </div>
                     </div>
                 </div>
+                
             </div>
         );
     }
 
     renderProjects() {
         return (
-            <div className="projects">
+            <div className="Home">
                 <PageHeader>Your Projects</PageHeader>
-                <ListGroup>
+                <div className="row text-center">
                     {!this.state.isLoading && this.renderProjectsList(this.state.projects)}
-                </ListGroup>
+                </div>
             </div>
         );
     }
@@ -98,7 +112,7 @@ export default class Home extends Component {
     render() {
         return (
             <div className="Home">
-                {this.renderLander()}
+                {this.props.isAuthenticated ? this.renderProjects() : this.renderLander()}
             </div>
         );
     }
